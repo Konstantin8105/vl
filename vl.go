75ad1e25e4d460f03c75cba9c1226a4e7954c0fe
konstantin8105 <konstantin8105@gmail.com>
2026-05-07 15:56:02 +0300

    step

--- vl.go
+++ vl.go
@@ -2019,16 +2019,6 @@ func (f *Frame) Render(width uint, drg Drawer) (height uint) {
 	// draw root widget
 	if f.root != nil {
 		// TODO create empty cell at background
-		//
-		// _ = f.root.Render(width-2*f.offsetRoot.col, DrawerLimit(
-		// 	func(row, col uint, s tcell.Style, r rune) {
-		// 		// create empty background for menu
-		// 		dr(row, col, s, r)
-		// 	},
-		// 	f.offsetRoot.row, f.offsetRoot.col,
-		// 	0, maxSize,
-		// 	0, width-2*f.offsetRoot.col+1,
-		// ))
 		h := f.root.Render(width-2*f.offsetRoot.col, DrawerLimit(
 			dr,
 			int(f.offsetRoot.row), int(f.offsetRoot.col),
